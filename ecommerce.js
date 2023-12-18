06e61dba485e9c052353342cddcbccba64bfd9ff
Kshitij Sarve <129431199+Kshitij10000@users.noreply.github.com>
2023-12-18 18:04:55 +0530

    Update ecommerce.js

--- ecommerce.js
+++ ecommerce.js
@@ -1,9 +1,33 @@
+
 const close = document.querySelector(".close");
 const open = document.querySelector(".ham");
 const menu = document.querySelector(".menu");
+
 close.addEventListener("click", () => {
   menu.style.visibility = "hidden";
 });
+
 open.addEventListener("click", () => {
   menu.style.visibility = "visible";
+});
+
+
+const productCards = document.querySelectorAll('.items');
+productCards.forEach(card => {
+  card.addEventListener('mouseover', () => {
+    card.style.transform = 'scale(1.05)';
+    card.style.transition = 'transform 0.3s';
+  });
+
+  card.addEventListener('mouseout', () => {
+    card.style.transform = 'scale(1)';
+  });
+});
+
+
+const buyNowButtons = document.querySelectorAll(".buy-now-btn");
+buyNowButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    alert("Product added to cart! Check out on Amazon.");
+  });
 });
